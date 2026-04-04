0bace2f896c7987f6d1b189900b3fc586b3eea18
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-04-04 10:54:37 +0800

    Update AttackEvent.java

--- src/main/java/myau/events/AttackEvent.java
+++ src/main/java/myau/events/AttackEvent.java
@@ -10,21 +10,5 @@ public class AttackEvent implements Event {
     private final Entity target;
     @Setter
     private boolean cancelled;
-
-    public AttackEvent(Entity target) {
-        this.target = target;
-        this.cancelled = false;
-    }
-
-    public Entity getTarget() {
-        return this.target;
-    }
-
-    public boolean isCancelled() {
-        return this.cancelled;
-    }
-
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
     }
 }
