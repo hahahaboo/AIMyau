39d46b124bdb127fe91c63ee56377f2413dc8008
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-05-18 21:16:04 +0800

    Update WorldTime.java

--- src/main/java/myau/module/modules/WorldTime.java
+++ src/main/java/myau/module/modules/WorldTime.java
@@ -1,35 +1,50 @@
 package myau.module.modules;
 
 import myau.event.EventTarget;
+import myau.event.events.PacketEvent;
 import myau.event.types.EventType;
 import myau.events.TickEvent;
 import myau.module.Category;
 import myau.module.Module;
 import myau.property.properties.IntProperty;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.server.S03PacketTimeUpdate;
 import net.minecraft.world.World;
 
 public class WorldTime extends Module {
     
-    public final IntProperty time = new IntProperty("time", 6000, 1, 24000);
+    public final IntProperty time = new IntProperty("time", 1000, 1, 24000);
 
     public WorldTime() {
-        super("WorldTime", "改變玩家端的天空時間 (client-side only)", Category.WORLD, 0, false, false);
+        super("WorldTime", "改變玩家端的天空時間 (client-side only)", Category.RENDER, 0, false, false);
+    }
+
+    @EventTarget
+    public void onPacket(PacketEvent event) {
+        if (!this.isEnabled() || event.getType() != EventType.RECEIVE) {
+            return;
+        }
+        if (event.getPacket() instanceof S03PacketTimeUpdate) {
+            event.setCancelled(true);
+        }
     }
 
     @EventTarget
     public void onTick(TickEvent event) {
-        if (!this.isEnabled() || event.getType() != EventType.PRE || Minecraft.getMinecraft().theWorld == null) {
+        if (!this.isEnabled() || event.getType() != EventType.PRE) {
             return;
         }
-        // 只在啟用時強制設定時間
-        ((World) Minecraft.getMinecraft().theWorld).setWorldTime(this.time.getValue());
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.theWorld != null) {
+            ((World) mc.theWorld).setWorldTime(this.time.getValue());
+        }
     }
 
     @Override
     public void onEnabled() {
-        if (Minecraft.getMinecraft().theWorld != null) {
-            ((World) Minecraft.getMinecraft().theWorld).setWorldTime(this.time.getValue());
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.theWorld != null) {
+            ((World) mc.theWorld).setWorldTime(this.time.getValue());
         }
     }
 }
