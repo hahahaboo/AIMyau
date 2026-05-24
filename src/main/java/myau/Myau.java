5ab07c4df351e182fe603901b881aa084cdf117c
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-05-24 16:02:48 +0800

    remove fuxking axx kbdelay
    * Delete src/main/java/myau/module/modules/KnockbackDelay.java
    
    * Update Myau.java

--- src/main/java/myau/Myau.java
+++ src/main/java/myau/Myau.java
@@ -94,7 +94,6 @@ public class Myau {
         moduleManager.modules.put(JumpReset.class, new JumpReset());
         moduleManager.modules.put(KeepSprint.class, new KeepSprint());
         moduleManager.modules.put(KillAura.class, new KillAura());
-        moduleManager.modules.put(KnockbackDelay.class, new KnockbackDelay());
         moduleManager.modules.put(LagRange.class, new LagRange());
         moduleManager.modules.put(LongJump.class, new LongJump());
         moduleManager.modules.put(MCF.class, new MCF());
