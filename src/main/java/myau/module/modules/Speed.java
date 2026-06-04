9cf6d2eb12c5ac03f8d1aa8f63765d33ef37e608
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-06-04 19:53:20 +0800

    Update Speed.java

--- src/main/java/myau/module/modules/Speed.java
+++ src/main/java/myau/module/modules/Speed.java
@@ -12,7 +12,6 @@ import myau.management.RotationState;
 import myau.mixin.IAccessorEntity;
 import myau.module.Module;
 import myau.module.Category;
-import myau.module.modules.KillAura;
 import myau.util.MoveUtil;
 import myau.util.RotationUtil;
 import myau.property.properties.BooleanProperty;
@@ -49,7 +48,7 @@ public class Speed extends Module {
     private boolean canBoostLegit() {
         if (!canBoost()) return false;
         KillAura killAura = (KillAura) Myau.moduleManager.modules.get(KillAura.class);
-        if (killAura.isEnabled() && killAura.target != null) return false;
+        if (killAura.isEnabled() && killAura.getTarget != null) return false;
         if (this.onlyJumping.getValue()) {
             return mc.gameSettings.keyBindJump.isKeyDown();
         }
