a0df403c21d401a3b291c77e39c8b7667d4f1974
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-07-06 10:54:48 +0800

    Add ClientSpoofer module to module manager

--- src/main/java/myau/Myau.java
+++ src/main/java/myau/Myau.java
@@ -78,6 +78,7 @@ public class Myau {
         moduleManager.modules.put(ChestESP.class, new ChestESP());
         moduleManager.modules.put(ChestStealer.class, new ChestStealer());
         moduleManager.modules.put(ClickGUIModule.class, new ClickGUIModule());
+        moduleManager.modules.put(ClientSpoofer.class, new ClientSpoofer());
         moduleManager.modules.put(Eagle.class, new Eagle());
         moduleManager.modules.put(ESP.class, new ESP());
         moduleManager.modules.put(FastPlace.class, new FastPlace());
