92829a82cce0227ca92dd8e258af050ffad191f8
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-08-01 19:32:48 +0800

    Change OAuth login status message

--- src/main/java/myau/management/altmanager/AltManagerGui.java
+++ src/main/java/myau/management/altmanager/AltManagerGui.java
@@ -530,7 +530,7 @@ public class AltManagerGui extends GuiScreen {
     }
 
     private void startOAuthLogin() {
-        status = "§6Opening browser...";
+        status = "§alink copied to clipboard!";
         MicrosoftOAuthTranslation.getRefreshToken(refreshToken -> mc.addScheduledTask(() -> {
             if (refreshToken == null || refreshToken.isEmpty()) {
                 status = "§cOAuth cancelled";
