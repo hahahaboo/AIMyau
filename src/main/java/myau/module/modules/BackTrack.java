b282cc7fac8a77a5d84db30dfb854db1994281f2
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-08-12 11:33:44 +0800

    Refactor onUpdate method for clarity and functionality

--- src/main/java/myau/module/modules/BackTrack.java
+++ src/main/java/myau/module/modules/BackTrack.java
@@ -153,20 +153,23 @@ public class BackTrack extends Module {
 
     @EventTarget
     public void onUpdate(UpdateEvent event){
-       if (!isEnabled() || target == null || realTargetPos == null || event.getType() != EventType.PRE) {
-          return;
-       }
+        if (!isEnabled() || target == null || realTargetPos == null || event.getType() != EventType.PRE) {
+            return;
+        }
 
-       float size = target.getCollisionBorderSize();
-       double width = target.width / 2.0 + size;
-       double height = target.height + size;
-       AxisAlignedBB aabb = new AxisAlignedBB(
-               realTargetPos.xCoord - width, realTargetPos.yCoord, realTargetPos.zCoord - width,
-               realTargetPos.xCoord + width, realTargetPos.yCoord + height, realTargetPos.zCoord + width
-       );
-       if (rayTrance.getValue() && RotationUtil.rayTrace(aabb, event.getNewYaw(), event.getNewPitch(), maxDistance.getValue()) == null) {
-           resetAndRelease();
-       }
+        float size = target.getCollisionBorderSize();
+        double width = target.width / 2.0 + size;
+        double height = target.height + size;
+        AxisAlignedBB aabb = new AxisAlignedBB(
+                realTargetPos.xCoord - width, realTargetPos.yCoord, realTargetPos.zCoord - width,
+                realTargetPos.xCoord + width, realTargetPos.yCoord + height, realTargetPos.zCoord + width
+        );
+        if (rayTrance.getValue() && RotationUtil.rayTrace(aabb, event.getNewYaw(), event.getNewPitch(), maxDistance.getValue()) == null) {
+            currentLatency = 0;
+            releaseAll();
+            target = null;
+            vec3 = null;
+        }
     }
     
     @EventTarget
