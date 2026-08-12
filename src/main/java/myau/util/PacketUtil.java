45aceb55cd535599ba1607df88cecbcad847733f
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-08-12 08:49:56 +0800

    Update PacketUtil.java

--- src/main/java/myau/util/PacketUtil.java
+++ src/main/java/myau/util/PacketUtil.java
@@ -1,13 +1,20 @@
 package myau.util;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.INetHandler;
 import net.minecraft.network.Packet;
+import net.minecraft.network.ThreadQuickExitException;
+import net.minecraft.network.play.INetHandlerPlayClient;
 
 public class PacketUtil {
     private static final Minecraft mc = Minecraft.getMinecraft();
 
     public static void sendPacket(Packet<?> packet) {
         mc.getNetHandler().getNetworkManager().sendPacket(packet);
+    }
+
+    public static void sendPacketNoEvent(Packet<?> packet) {
+        mc.getNetHandler().getNetworkManager().sendPacket(packet, null);
     }
     public static void receivePacket(Packet<?> packet) {
         if (packet == null)
@@ -19,7 +26,7 @@ public class PacketUtil {
         }
     }
     @SuppressWarnings("unchecked")
-    public static void sendPacketNoEvent(Packet<?> packet) {
-        mc.getNetHandler().getNetworkManager().sendPacket(packet, null);
+    public static <H extends INetHandler> Packet<H> castPacket(Packet<?> packet) throws ClassCastException {
+        return (Packet<H>) packet;
     }
 }
