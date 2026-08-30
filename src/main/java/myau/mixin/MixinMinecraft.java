feebdbab8490dfe32bfc4b52c7148d95323f3c28
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-08-30 09:41:46 +0800

    Update MixinMinecraft.java

--- src/main/java/myau/mixin/MixinMinecraft.java
+++ src/main/java/myau/mixin/MixinMinecraft.java
@@ -128,7 +128,12 @@ public abstract class MixinMinecraft {
         EventManager.call(event);
         if (event.isCancelled()) {
             callbackInfo.cancel();
-            this.playerController.resetBlockRemoving();
+            // 只有 AbortBreaking 沒開時才重置
+            if (Myau.moduleManager == null
+                    || Myau.moduleManager.modules.get(AbortBreaking.class) == null
+                    || !Myau.moduleManager.modules.get(AbortBreaking.class).isEnabled()) {
+                this.playerController.resetBlockRemoving();
+            }
         }
     }
 
