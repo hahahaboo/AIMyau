5805da664dfbee2da0a4a3d9262c0f23d1767b38
hahahaboo <max072001@apps.ntpc.edu.tw>
2026-09-17 21:36:04 +0800

    Add FreeCam module to module manager

--- src/main/java/myau/Myau.java
+++ src/main/java/myau/Myau.java
@@ -88,6 +88,7 @@ public class Myau {
         moduleManager.modules.put(ESP.class, new ESP());
         moduleManager.modules.put(FastPlace.class, new FastPlace());
         moduleManager.modules.put(Fly.class, new Fly());
+        moduleManager.modules.put(FreeCam.class, new FreeCam());
         moduleManager.modules.put(FreeLook.class, new FreeLook());
         moduleManager.modules.put(FullBright.class, new FullBright());
         moduleManager.modules.put(GhostHand.class, new GhostHand());
